8dd3685d9204ab3b9cbf369efbda1da887264d35
Miguel-Rocha <miguel_rocha00715@hotmail.com>
2016-03-02 20:20:12 -0600

    Terminando la clase de conexiona a una base de datos específica Además se reemplazo la extención del archivo base de datos

--- src/com/rochavieyra/datos/Conexion.java
+++ src/com/rochavieyra/datos/Conexion.java
@@ -4,11 +4,37 @@
  * and open the template in the editor.
  */
 package com.rochavieyra.datos;
-
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.*;
 /**
  *
  * @author Miguel Rocha
  */
 public class Conexion {
+    Connection conexion;
+    Statement consulta;
+    public String ruta;
     
+    public Conexion(){
+        String ruta1 = System.getProperty("user.dir");
+        JOptionPane.showMessageDialog(null, "La ruta de la base de datos es:\n"+ ruta1);
+        ruta = "/"+ruta1+"/Horarios.db";
+    }
+    public void conectar(){
+        try {
+            Class.forName("org.sqlite.JDBC");
+        } catch (ClassNotFoundException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+        try {
+            conexion = DriverManager.getConnection("jdbc:sqlite"+ruta);
+            consulta = conexion.createStatement();
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
 }
